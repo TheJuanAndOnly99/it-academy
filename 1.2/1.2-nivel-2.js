b83f3f3ddcb49d02f40a9793256a20e1c210736e
Juan Estrella <juan.estrella@finos.org>
2023-04-24 12:38:25 +0200

    implement feedback

--- 1.2/1.2-nivel-2.js
+++ 1.2/1.2-nivel-2.js
@@ -2,7 +2,7 @@
 // Exercici 1
 // Crea una arrow function que, rebent un paràmetre, retorni un objecte amb un atribut que tingui com a valor el paràmetre rebut.
 
-createPerson = name => ({ name: name });
+const createPerson = name => ({ name: name });
 
 const person1 = createPerson('Juan');
 
@@ -11,18 +11,18 @@ console.log(person1);
 // Exercici 2
 // Crea una classe "Persona" que rebi un paràmetre 'nom' en ser instanciada. La classe inclourà un mètode dirNom que imprimeixi per consola el paràmetre 'nom'. Invoca el mètode dirNom des de fora de la classe.
 
-class Persona {
-  constructor(nom) {
-    this.nom = nom;
+class Person {
+  constructor(name) {
+    this.name = name;
   }
   
-  dirNom() {
-    console.log(`Hola, em dic ${this.nom}!`);
+  sayName() {
+    console.log(`Hola, em dic ${this.name}!`);
   }
 }
 
 // Create object 
-const juan = new Persona("Juan");
+const juan = new Person("Juan");
 
 // Call dirNom() method on object juan
-juan.dirNom();
+juan.sayName();
--- 1.2/1.2-nivel-3.js
+++ 1.2/1.2-nivel-3.js
@@ -4,39 +4,52 @@
 // Las clases abstractas se pueden definir como clases que no se pueden instanciar.
 // No hay una palabra clave "abstract" en JavaScript para declarar una clase como una clase abstracta.
 
-function createPerson(fName, lName, age) {
-  class Person  {
-    constructor(fName, lName, age) {
-      this.fName = fName;
-      this.lName = lName;
-      this.age = age;
-      
-    if (this.constructor === Person) {
-      throw new Error('You can not create an instance of Abstract Class'); 
-      }
-    }
-      
-    sayName(){
-      throw new Error('Abstract method makeSound() must be overridden in subclasses.');
+class Person  {
+  constructor(fName, lName, age) {
+    this.fName = fName;
+    this.lName = lName;
+    this.age = age;
+    
+  if (this.constructor === Person) {
+    throw new Error('You can not create an instance of an Abstract Class.'); 
     }
   }
-
-  class Teacher extends Person {
-    constructor(fName, lName, age) {
-        super(fName, lName, age)
-    }
-
-    sayName(){
-      console.log(`Hello my name is ${this.fName}`)
-    }
+    
+  sayName(){
+    throw new Error('Abstract method sayName() must be overridden in subclasses.');
   }
 
-  return new Teacher(fName, lName, age);
-  
+  check(){
+    throw new Error('Abstract method check() must be overridden in subclasses.');
+  }
 }
 
-const david = createPerson("Dave", "Stevenson", 25);
-const steve = createPerson("Steve", "Davidson", 22);
+class Teacher extends Person {
+  constructor(fName, lName, age) {
+    super(fName, lName, age)
+  }
 
-console.log(david);
-david.sayName();
+  sayName(){
+    console.log(`Hello my name is ${this.fName}`)
+  }
+
+  check(){
+    console.log(this instanceof Person)
+    console.log(this instanceof Teacher)
+  }
+}
+
+function createPersonObject(fName, lName, age) {
+  return new Teacher(fName, lName, age)
+}
+
+const patrick = createPersonObject('Patrick', 'Star', 24);
+const bob = createPersonObject('Spongebob', 'Squarepants', 22);
+
+patrick.check();
+patrick.sayName();
+
+bob.check();
+bob.sayName();
+
+
