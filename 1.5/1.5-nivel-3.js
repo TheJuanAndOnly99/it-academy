a4ba6dceb2b14e48572e54612850a78f17ea2546
Juan Estrella <juan.estrella@finos.org>
2023-04-24 11:38:39 +0200

    entrage 1.5 nivel 3

--- 1.5/1.5-nivel-3.js
+++ 1.5/1.5-nivel-3.js
@@ -56,11 +56,11 @@ const createEncodedFiles = async (filePath) => {
       console.log(`File encoded in base64: ${base64FilePath}`);;
     }
   } catch (err) {
-    console.log(err.message);
+    console.log(`File ${filePath} does not exist`);
   }
 };
 
-const filePath = './1.5/1.5-nivel-1.js';
+const filePath = './1.5/test.js';
 const fileName = filePath.split('/').pop().split('.').slice(0, -1).join('.');
 
 createEncodedFiles(filePath)
@@ -68,11 +68,17 @@ createEncodedFiles(filePath)
 
 // Crea una funció que guardi els fitxers del punt anterior, ara encriptats amb l'algoritme aes-192-cbc, i esborri els fitxers inicials.
 
+// Encryption settings
+const algorithm = 'aes-192-cbc';
+const password = process.env.PASSWORD;
+const key = crypto.scryptSync(password, 'salt', 24);
+const iv = Buffer.alloc(16, 0);
 
 const encryptFile = (filePath, algorithm, key, iv) => {
   return new Promise((resolve, reject) => {
+    const fileName = filePath.split(/\.txt$/)[0];
     const input = fs.createReadStream(filePath);
-    const output = fs.createWriteStream(filePath + '.enc');
+    const output = fs.createWriteStream(fileName + '.enc');
 
     const cipher = crypto.createCipheriv(algorithm, key, iv);
     input.pipe(cipher).pipe(output);
@@ -90,6 +96,9 @@ const encryptFile = (filePath, algorithm, key, iv) => {
   });
 };
 
+const hexFilePath = './1.5/test-encoded-hex.txt';
+const base64FilePath = './1.5/test-encoded-base64.txt';
+
 const encryptEncodedFiles = async (hexFilePath, base64FilePath, algorithm, key, iv) => {
   try {
     await fs.promises.access(hexFilePath, fs.constants.F_OK);
@@ -105,22 +114,120 @@ const encryptEncodedFiles = async (hexFilePath, base64FilePath, algorithm, key, 
       console.log(`File ${encryptedBase64FilePath} encrypted!`);
     }
   } catch (err) {
-    console.log(err.message);
+    console.log('Encoded files not found, cannot encrypt');
   }
 };
-
-const hexFilePath = './1.5/1.5-nivel-1-encoded-hex.txt';
-const base64FilePath = './1.5/1.5-nivel-1-encoded-base64.txt';
-
-// Encryption settings
-const algorithm = 'aes-192-cbc';
-const password = process.env.PASSWORD;
-const key = crypto.scryptSync(password, 'salt', 24);
-const iv = Buffer.alloc(16, 0);
 
 encryptEncodedFiles(hexFilePath, base64FilePath, algorithm, key, iv);
 
 // Crea una altra funció que desencripti i descodifiqui els fitxers de l'apartat anterior tornant a generar una còpia de l'inicial.
 
+const decryptFile = (filePath, algorithm, key, iv) => {
+  return new Promise((resolve, reject) => {
+    const fileName = filePath.split(/\.enc$/)[0];
+    const input = fs.createReadStream(filePath);
+    const output = fs.createWriteStream(fileName + '.txt');
+
+    const decipher = crypto.createDecipheriv(algorithm, key, iv);
+    input.pipe(decipher).pipe(output);
+
+    output.on('finish', () => {
+      fs.unlink(filePath, (err) => {
+        if (err) reject(err);
+        else resolve(output.path);
+      });
+    });
+
+    decipher.on('error', (err) => {
+      reject(err);
+    });
+  });
+};
+
+// Decode Hex file
+const inputFilePathHex = './1.5/test-encoded-hex.txt'; 
+const outputFilePathHex = './1.5/test-hex-decoded.js'
+
+function decodeHexFile(inputFilePathHex, outputFilePathHex) {
+  fs.access(inputFilePathHex, fs.constants.F_OK, (err) => {
+    if (err) {
+      console.log(`File ${inputFilePathHex} does not exist`);
+    } else {
+      fs.readFile(inputFilePathHex, (err, data) => {
+        if (err) throw err;
+      
+        // Decode hexadecimal data
+        const decodedData = Buffer.from(data.toString(), 'hex');
+      
+        fs.writeFile(outputFilePathHex, decodedData, (err) => {
+          if (err) throw err;
+          console.log('File successfully decoded and written to', outputFilePathHex);
+        });
+      
+        // Delete input file
+        fs.unlink(inputFilePathHex, (err) => {
+          if (err) throw err;
+          console.log('Input file', inputFilePathHex, 'deleted successfully');
+        });
+      });
+    }
+  });
+}
+
+// Decode base64 file
+const inputFilePathBase64 = './1.5/test-encoded-base64.txt'; 
+const outputFilePathBase64 = './1.5/test-base64-decoded.js'
+
+function decodeBase64File(inputFilePathBase64, outputFilePathBase64) {
+  fs.access(inputFilePathBase64, fs.constants.F_OK, (err) => {
+    if (err) {
+      console.log(`File ${inputFilePathBase64} does not exist`);
+    } else {
+      fs.readFile(inputFilePathBase64, (err, data) => {
+        if (err) throw err;
+      
+       // Decode base64 data
+        const decodedData = Buffer.from(data.toString(), 'base64');
+      
+        fs.writeFile(outputFilePathBase64, decodedData, (err) => {
+          if (err) throw err;
+          console.log('File successfully decoded and written to', outputFilePathBase64);
+        });
+      
+       // Delete input file
+        fs.unlink(inputFilePathBase64, (err) => {
+          if (err) throw err;
+          console.log('Input file', inputFilePathBase64, 'deleted successfully');
+        });
+      });
+    }
+  });
+}  
+
+const decryptEncodedFiles = async (encryptedHexFilePath, encryptedBase64FilePath, algorithm, key, iv) => {
+  try {
+    await fs.promises.access(encryptedHexFilePath, fs.constants.F_OK);
+    await fs.promises.access(encryptedBase64FilePath, fs.constants.F_OK);
+
+    const decryptedHexFilePath = await decryptFile(encryptedHexFilePath, algorithm, key, iv);
+    if (decryptedHexFilePath) {
+      decodeHexFile(inputFilePathHex, outputFilePathHex);
+      console.log(`File: ${decryptedHexFilePath} decrypted!`);
+    }
+
+    const decryptedBase64FilePath = await decryptFile(encryptedBase64FilePath, algorithm, key, iv);
+    if (decryptedBase64FilePath) {
+      decodeBase64File(inputFilePathBase64, outputFilePathBase64);
+      console.log(`File: ${decryptedBase64FilePath} decrypted!`);
+    }
+  } catch (err) {
+    console.log('Encoded files not found, cannot decrypt');
+  }
+};
+
+const encryptedHexFilePath = './1.5/test-encoded-hex.enc';
+const encryptedBase64FilePath = './1.5/test-encoded-base64.enc';
+
+decryptEncodedFiles(encryptedHexFilePath, encryptedBase64FilePath, algorithm, key, iv);
 
 // Inclou un README amb instruccions per a l'execució de cada part.
---
+++ 1.5/readme.md
@@ -0,0 +1,19 @@
+# Readme
+
+## Description
+This code contains a set of functions to encode, encrypt, and decrypt files in Node.js using the built-in `fs` module and the `crypto` module. The code demonstrates how to create encoded files in hexadecimal and base64 formats from an input file, encrypt the encoded files using the AES-192-CBC algorithm, and then decrypt and decode the encrypted files back to their original format.
+
+## Installation
+1. Install Node.js on your machine, if not already installed.
+2. Clone or download the code from the repository.
+3. Navigate to the directory containing the code in your terminal or command prompt.
+4. Install the required dependencies by running the following command: `npm install dotenv`
+
+## Usage
+1. Modify the `.env` file with your desired password for encryption. Make sure to keep the password secure as it will be used for encryption and decryption.
+2. Open the `1.5-nivel-3.js` file in a code editor.
+3. Update the `filePath` variable with the path to the input file that you want to encode, encrypt, and decrypt.
+4. Run the code using the command `node 1.5-nivel-3.js` in your terminal or command prompt.
+  - The first time the code is run it will create two new files encoded in Hex and Base64 respectively.
+  - The second time the code is run it will encrypt the encoded files using the AES-192-CBC algorithm.
+  - The third time the code is run it will decrypt and decode the files.
