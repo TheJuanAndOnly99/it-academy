85a761a09dd16efe1660e5baf1d01d08bed2d8c0
Juan Estrella <juan.estrella@finos.org>
2023-04-25 10:37:15 +0200

    import function

--- 1.3/1.3-nivel-3.js
+++ 1.3/1.3-nivel-3.js
@@ -1,8 +1,8 @@
 // Nivel 3
 // Fixa un element catch a la invocació del nivell anterior que capturi qualsevol error i el mostri per la consola.
-const module = require ('./1.3-nivel-2');
+const {getEmployee} = require ('./1.3-nivel-2');
 
-module.getEmployee(6)
+getEmployee(6)
   .then(res => getSalary(res))
     .then(res => console.log(res))
     .catch(err => console.log(err.message))
