a4216a5d836bb552b611cb4221cd83d39ac26c86
Juan Estrella <juan.estrella@finos.org>
2023-05-02 09:22:47 +0200

    export functions

--- 1.3/1.3-nivel-1.js
+++ 1.3/1.3-nivel-1.js
@@ -13,13 +13,13 @@ function createPromise(bool) {
   })
 }
 
-createPromise(true)
-  .then(res => console.log(res))
-  .catch(err => console.log(err.message));
+// createPromise(true)
+//   .then(res => console.log(res))
+//   .catch(err => console.log(err.message));
 
-createPromise(false)
-  .then(res => console.log(res))
-  .catch(err => console.log(err.message));
+// createPromise(false)
+//   .then(res => console.log(res))
+//   .catch(err => console.log(err.message));
 
 // Exercici 2 
 // Crea una arrow function que rebi un paràmetre i una funció callback i li passi a la funció un missatge o un altre (que s'imprimirà per consola) en funció del paràmetre rebut.
@@ -30,9 +30,13 @@ const voterStatus = vote => {
 
 const checkVoterStatus = (age, callBack) => {
   const canVote = age < 18 ? false : true;
-  callBack(canVote);
+  callBack(canVote)
+  return canVote
 }
 
 checkVoterStatus(18, voterStatus)
 checkVoterStatus(7, voterStatus)
+
+module.exports.voterStatus = voterStatus;
+module.exports.checkVoterStatus = checkVoterStatus;
 
