b0aa08608eb14760fe16ecffa85c2fb85c0b3341
Juan Estrella <juan.estrella@finos.org>
2023-05-04 11:50:43 +0200

    refactor abstract class to use Class.prototype

--- 1.2/1.2-nivel-3.js
+++ 1.2/1.2-nivel-3.js
@@ -5,55 +5,31 @@
 // No hay una palabra clave "abstract" en JavaScript para declarar una clase como una clase abstracta.
 
 class Person  {
-  constructor(fName, lName, age) {
-    this.fName = fName;
-    this.lName = lName;
-    this.age = age;
+  constructor(firstName) {
+    this.firstName = firstName;
     
   if (this.constructor === Person) {
     throw new Error('You can not create an instance of an Abstract Class.'); 
     }
   }
-    
-  sayName(){
-    throw new Error('Abstract method sayName() must be overridden in subclasses.');
-  }
 
-  check(){
-    throw new Error('Abstract method check() must be overridden in subclasses.');
-  }
 }
 
-class Teacher extends Person {
-  constructor(fName, lName, age) {
-    super(fName, lName, age)
-  }
-
-  sayName(){
-    console.log(`Hello my name is ${this.fName}`)
-  }
-
-  check(){
-    console.log(this instanceof Person)
-    console.log(this instanceof Teacher)
-  }
+function createPersonObject(firstName) {
+  const human = Object.create(Person.prototype);
+  human.firstName = firstName;
+  return human;
 }
 
-function createPersonObject(fName, lName, age) {
-  return new Teacher(fName, lName, age)
-}
-
-const patrick = createPersonObject('Patrick', 'Star', 24);
-const bob = createPersonObject('Spongebob', 'Squarepants', 22);
-
-patrick.check();
-patrick.sayName();
-
-bob.check(); // returns true for both the Person & Teacher class
-bob.sayName();
+const patrick = createPersonObject('Patrick');
+patrick instanceof Person
+const bob = createPersonObject('Spongebob');
+bob instanceof Person
 
 module.exports.Person = Person;
-module.exports.Teacher = Teacher;
+module.exports.createPersonObject = createPersonObject;
+
+
 
 
 
